dcb65e3e4a5e8a56f5a5a128c7f0cfec7560ce00
bimasatya24 <bimasatyanugraha@gmail.com>
2026-07-30 06:58:55 +0700

    Membuat Navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
 @include('layout.header')
-    
+    <ul class="list-none m-0 p-0 bg-black flex justify-center">
+        <li class="float-left"><a href="" class="block text-white px-4 py-[14px] no-underline hover:bg-blue-600">Tabel Kurs Real-Time</a></li>
+        <li class="float-left"><a href="" class="block text-white px-4 py-[14px] no-underline hover:bg-blue-600">Company Profile</a></li>
+        <li class="float-left"><a href="" class="block text-white px-4 py-[14px] no-underline hover:bg-blue-600">Informasi Perusahaan</a></li>
+    </ul>
 @include('layout.footer')
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,7 +7,7 @@
     <title>PT Bina Sukses Valasindo</title>
 </head>
 <body class="bg-gray-50 text-gray-900">
-    <header class="bg-blue-600 text-white py-3 mb-4">
+    <header class="bg-blue-600 text-white py-3">
         <div class="container mx-auto px-4 text-center">
             <h1 class="font-verdana text-[32px] font-bold">PT Bina Sukses Valasindo</h1>
             <p class="font-verdana text-[14px]">Layanan Penukaran Valuta Asing Terpercaya</p>
