e5a811a58eb3380a5c3987f9cf192a05615048a0
bimasatya24 <bimasatyanugraha@gmail.com>
2026-07-29 20:21:46 +0700

    Membuat halaman baru bernama display dan penambahan routing baru di web.php

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\UploadController;
 use App\Models\Upload;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function() {
+    return view('home');
+})->name('home');
+
+Route::get('/display', function () {
     $allUpload = Upload::all();
 
     $lastUpdated = Upload::latest('updated_at')->first()?->updated_at?->translatedFormat('d F Y H:i');
 
-    return view('home', compact('allUpload', 'lastUpdated'));
-});
+    return view('display.index', compact('allUpload', 'lastUpdated'));
+})->name('display.index');
 
 Route::resource('upload', UploadController::class);
