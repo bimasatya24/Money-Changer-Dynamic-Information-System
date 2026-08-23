4eaa5a86d959ddef5e46aeb68750f13aa2e9caba
bimasatya24 <bimasatyanugraha@gmail.com>
2026-08-23 13:16:15 +0700

    Menambahkan gambar BSV dan KUPVA Berizin pada header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,17 +1,55 @@
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>PT Bina Sukses Valasindo</title>
 </head>
+
 <body class="bg-gray-50 text-gray-900">
     <header class="bg-blue-600 text-white py-3 relative">
         <div class="container mx-auto px-4 text-center">
-            <h1 class="font-verdana text-[32px] font-bold">PT Bina Sukses Valasindo</h1>
-            <p class="font-verdana text-[14px]">{{ __('Layanan Penukaran Valuta Asing Terpercaya') }}</p>
-            <p>Jl. Proklamator Raya No. 331 (Area Belakang Bank Mandiri), Bandar Jaya Timur, Kec. Terbanggi Besar, Kab. Lampung Tengah, Lampung 34163</p>
-            <p>{{ __('Telepon / WhatsApp:') }} +62 857-8951-0332 / +62 853-6966-7788</p>
+            <div class="flex items-center justify-between min-h-20">
+                {{-- Logo BSV --}}
+                <div class="flex items-center">
+                    <img
+                        src="{{ asset('images\WhatsApp Image 2026-08-20 at 14.41.18.jpeg') }}"
+                        alt="Logo BSV"
+                        class="h-14 w-auto object-contain">
+                </div>
+
+                {{-- Informasi Perusahaan --}}
+                <div class="text-center flex-1 px-6">
+                    <h1 class="font-verdana text-[32px] font-bold">
+                        PT Bina Sukses Valasindo
+                    </h1>
+
+                    <p class="font-verdana text-[14px]">
+                        {{ __('Layanan Penukaran Valuta Asing Terpercaya') }}
+                    </p>
+
+                    <p>
+                        Jl. Proklamator Raya No. 331 (Area Belakang Bank Mandiri),
+                        Bandar Jaya Timur, Kec. Terbanggi Besar,
+                        Kab. Lampung Tengah, Lampung 34163
+                    </p>
+
+                    <p>
+                        {{ __('Telepon / WhatsApp:') }}
+                        +62 857-8951-0332 / +62 853-6966-7788
+                    </p>
+                </div>
+
+                {{-- Logo KUPVA Berizin --}}
+                <div class="flex items-center">
+                    <img
+                        src="{{ asset('images\Logo.jpeg') }}"
+                        alt="Logo KUPVA Berizin"
+                        class="h-14 w-auto object-contain">
+                </div>
+            </div>
+
         </div>
     </header>
